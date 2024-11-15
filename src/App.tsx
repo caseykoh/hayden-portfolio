a30ae4673c1bd0f5eca4cdee2edbf2bec9f91533
Casey Koh <s.casey.koh@gmail.com>
2024-11-14 19:58:02 -0500

    Remove tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,9 @@ function App() {
   return (
     <>
       <div className="relative bg-slate-400 py-[16vw]" id="work">
-        <div className="group absolute top-[-6vw]">
-          <h1 className="whitespace-nowrap text-[25vw] md:text-[15vw] lg:text-[18vw] tracking-tighter w-full text-center font-sans font-medium text-white">
-            [Hayden Koh]
-          </h1>
-          <div className="absolute hidden group-hover:block bg-white text-black text-xs rounded p-2 top-full left-1/2 transform -translate-x-1/2 -translate-y-14 z-10">
-            Cinematographer and Camera Assistant based in Toronto, Canada.
-          </div>
-        </div>
+        <h1 className="absolute top-[-6vw] whitespace-nowrap text-[25vw] md:text-[15vw] lg:text-[18vw] tracking-tighter w-full text-center font-sans font-medium text-white">
+          [Hayden Koh]
+        </h1>
         <div className="mx-0 pb-12 md:mx-32 md:pb-24 min-h-screen relative z-3">
           <div className="absolute inset-0 bg-black"></div>
           <div className="relative z-10">
